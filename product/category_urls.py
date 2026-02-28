1ad24987770d5076ed09ad3b48be5b05a632f375
Athaher Sayem <athahersayem@outlook.com>
2026-03-01 00:54:25 +0600

    using related field , nested serializer , link a nested feild

--- product/category_urls.py
+++ product/category_urls.py
@@ -3,5 +3,6 @@ from product import views
 
 
 urlpatterns = [
-    path('', views.view_category , name='category-list')
+    path('', views.view_category , name='category-list'),
+    path('<int:pk>/', views.view_specific_category , name='view-specific-category'),
 ]
--- product/views.py
+++ product/views.py
@@ -4,7 +4,15 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from product.models import Product,Category
 from rest_framework import status
-from product.serializers import ProductSerializer 
+from product.serializers import ProductSerializer ,CategorySerializer
+
+
+@api_view()
+def view_products(request):
+      products = Product.objects.select_related('category').all()
+      serializer = ProductSerializer(products, many=True , context={'request': request})
+      return Response(serializer.data)
+
 
 @api_view()
 def view_specific_product(request,id):
@@ -16,3 +24,10 @@ def view_specific_product(request,id):
 @api_view()
 def view_category(request):
     return Response({'message':'Okey'})
+
+
+@api_view()
+def view_specific_category(request,pk):
+        category = get_object_or_404(Category,pk=pk)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
