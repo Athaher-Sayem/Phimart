a992061092d8575bb5691a25d0ab2d3b56c4a2a5
Athaher Sayem <athahersayem@outlook.com>
2026-03-01 00:22:46 +0600

    Added basic serializer

--- product/views.py
+++ product/views.py
@@ -1,20 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from product.models import Product,Category
 from rest_framework import status
+from product.serializers import ProductSerializer 
 
 @api_view()
 def view_specific_product(request,id):
-    try:
-        product = Product.objects.get(pk=id)
-        product_dict = {
-        'id': product.id , 'name' : product.name , 'price':product.price
-        }
-        return Response(product_dict)
-    except Product.DoesNotExist:
-        return Response({'message':"Product does not exists"},status=404)
+        product = get_object_or_404(Product,pk=id)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+   
 
 @api_view()
 def view_category(request):
