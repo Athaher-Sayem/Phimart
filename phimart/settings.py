76df3be86b2044c29c8c2aed12b7e89ed207b8e5
Athaher Sayem <athahersayem@outlook.com>
2026-03-02 18:34:35 +0600

    Setup JWT

--- phimart/settings.py
+++ phimart/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'rest_framework',
     'django_filters',
+    'djoser',
     'order',
     'users',
     'product',
@@ -137,4 +138,13 @@ REST_FRAMEWORK ={
     'COERCE_DECIMAL_TO_STRING':False,
     # 'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
     # 'PAGE_SIZE':10,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+       
+    ),
+}
+
+
+SIMPLE_JWT = {
+   'AUTH_HEADER_TYPES': ('JWT',),
 }
