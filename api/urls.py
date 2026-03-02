600efc43023e5470278f2418e77e6d2b69bb111c
Athaher Sayem <athahersayem@outlook.com>
2026-03-03 00:18:06 +0600

    Change auth to api urls

--- api/urls.py
+++ api/urls.py
@@ -23,6 +23,8 @@ cart_router.register('items', CartitemViewSet , basename='cart-item')
 urlpatterns = [
     path('', include(router.urls)),
     path('', include(product_router.urls)),
-    path('', include(cart_router.urls))
+    path('', include(cart_router.urls)),
+     path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
 
